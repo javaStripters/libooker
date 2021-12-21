0fd02257c21068b05206692380ef36dd470e1538
Степан Афанасьев <awanstep@ya.ru>
2021-12-22 02:57:43 +0300

    Added role restriction in r.t.l.controller.AuthController

--- src/main/java/ru/thecntgfy/libooker/controller/AuthController.java
+++ src/main/java/ru/thecntgfy/libooker/controller/AuthController.java
@@ -1,6 +1,7 @@
 package ru.thecntgfy.libooker.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import ru.thecntgfy.libooker.dto.JwtResponse;
 import ru.thecntgfy.libooker.dto.LoginDTO;
 import ru.thecntgfy.libooker.dto.RegisterDTO;
+import ru.thecntgfy.libooker.model.Role;
 import ru.thecntgfy.libooker.model.User;
 import ru.thecntgfy.libooker.model.UserMapper;
 import ru.thecntgfy.libooker.security.JwtProvider;
@@ -35,9 +38,15 @@ public class AuthController {
         return new JwtResponse(token);
     }
 
-    //TODO: REstrict roles
     @PostMapping("register")
     public User registerUser(@Valid @RequestBody RegisterDTO dto) {
+        Role role = dto.getRole();
+        if (!role.equals(Role.TUTOR) && !role.equals(Role.STUDENT))
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN,
+                    "Для создания аккаунта администратора обратитесь к действующему администратору"
+            );
+
         User user = UserMapper.registerDtoToUser(dto);
         return userService.createUser(user);
     }
